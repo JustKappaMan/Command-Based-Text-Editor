9a08418443001b862b22ff1469e7b7f2fc6cd253
Kirill Volozhanin <volozhaninkirill@gmail.com>
2023-03-16 19:21:43 +0500

    Some basic structure

---
+++ main.py
@@ -0,0 +1,37 @@
+
+
+def main():
+    pass
+
+
+class Document:
+    def __init__(self):
+        pass
+
+    def back_up(self):
+        pass
+
+    def insert_line(self):
+        pass
+
+    def delete_line(self):
+        pass
+
+    def swap_lines(self):
+        pass
+
+    def clear(self):
+        pass
+
+    def undo(self):
+        pass
+
+    def save(self):
+        pass
+
+    def close(self):
+        pass
+
+
+if __name__ == '__main__':
+    main()
