2e19cc84fb59cca6b1ff225236ff71ed6c3a3217
Kirill Volozhanin <volozhaninkirill@gmail.com>
2023-03-17 15:51:41 +0500

    Replaced: 'row' -> 'line'

--- main.py
+++ main.py
@@ -38,35 +38,35 @@ class Document:
     def number_of_lines(self) -> int:
         return len(self.current_content)
 
-    def insert_line(self, text: str, row: int = None, column: int = None) -> None:
+    def insert_line(self, text: str, line: int = None, column: int = None) -> None:
         self.back_up()
 
-        if row and column:
-            if 1 <= row <= self.number_of_lines:
-                if 1 <= column < len(self.current_content[row - 1]):
-                    row_content = self.current_content[row - 1]
-                    self.current_content[row - 1] = ''.join((row_content[:column], text, row_content[column:]))
-        elif row:
-            if 1 <= row <= self.number_of_lines:
-                self.current_content[row - 1] = self.current_content[row - 1].removesuffix('\n')
-                self.current_content[row - 1] += f'{text}\n'
+        if line and column:
+            if 1 <= line <= self.number_of_lines:
+                if 1 <= column < len(self.current_content[line - 1]):
+                    line_content = self.current_content[line - 1]
+                    self.current_content[line - 1] = ''.join((line_content[:column], text, line_content[column:]))
+        elif line:
+            if 1 <= line <= self.number_of_lines:
+                self.current_content[line - 1] = self.current_content[line - 1].removesuffix('\n')
+                self.current_content[line - 1] += f'{text}\n'
         else:
             if not self.current_content[-1].endswith('\n'):
                 self.current_content[-1] += '\n'
             self.current_content.append(text)
 
-    def delete_line(self, row: int = None) -> None:
-        if row:
-            if 1 <= row <= self.number_of_lines:
+    def delete_line(self, line: int = None) -> None:
+        if line:
+            if 1 <= line <= self.number_of_lines:
                 self.back_up()
-                del self.current_content[row - 1]
+                del self.current_content[line - 1]
             else:
-                sys.exit(f"Error! You can't delete the line №{row}. "
+                sys.exit(f"Error! You can't delete the line №{line}. "
                          f'The number of lines in the file: {self.number_of_lines}.')
         else:
             sys.exit('Error! You must specify the number of the line you want to delete.')
 
-    def swap_lines(self, row1: int, row2: int) -> None:
+    def swap_lines(self, line1: int, line2: int) -> None:
         pass
 
     def clear(self) -> None:
