50805d367a73e78abf8f9117bd49154d7640573b
Kirill Volozhanin <volozhaninkirill@gmail.com>
2023-03-19 17:15:21 +0500

    Fixed several errors in InputHandler

--- input_handler.py
+++ input_handler.py
@@ -10,7 +10,7 @@ class InputHandler:
 
     def start(self):
         while True:
-            user_input = input('>>> ')
+            self.user_input = input('>>> ')
 
             if m := re.match(r'insert(?: (\d+))?(?: (\d+))? \"(.+)\"$', self.user_input):
                 match len(args := [x for x in m.groups() if x is not None]):
@@ -20,18 +20,18 @@ class InputHandler:
                         self.document.insert_line(line_number=int(args[0]), text=args[1])
                     case 3:
                         self.document.insert_line(line_number=int(args[0]), column_number=int(args[1]), text=args[2])
-            elif m := re.match(r'delete (\d+)$', user_input):
+            elif m := re.match(r'delete (\d+)$', self.user_input):
                 self.document.delete_line(line_number=int(m.group(1)))
-            elif m := re.match(r'swap (\d+) (\d+)$', user_input):
+            elif m := re.match(r'swap (\d+) (\d+)$', self.user_input):
                 if len(args := [x for x in m.groups() if x is not None]) == 2:
                     self.document.swap_lines(int(args[0]), int(args[1]))
-            elif user_input == 'undo':
+            elif self.user_input == 'undo':
                 self.document.undo()
-            elif user_input == 'clear':
+            elif self.user_input == 'clear':
                 self.document.clear()
-            elif user_input == 'save':
+            elif self.user_input == 'save':
                 self.document.save()
-            elif user_input == 'close':
+            elif self.user_input == 'close':
                 self.document.close()
             else:
                 print('Error! Unknown command.')
