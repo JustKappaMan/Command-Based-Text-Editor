2173743597a227622effac5bd7a3462903486a36
Kirill Volozhanin <volozhaninkirill@gmail.com>
2023-03-19 05:12:40 +0500

    Massive changes
    * 'swap_lines' now adds newline characters to the end of selected lines if there aren't any
    * Added prototype of command prompt handler
    * Replaced: line -> line_number
    * Replaced: column -> column_number

--- main.py
+++ main.py
@@ -1,10 +1,38 @@
+import re
 import sys
 import copy
 import pathlib
 
 
 def main():
-    pass
+    document = Document()
+
+    while True:
+        user_input = input('>>> ')
+
+        if m := re.match(r'insert(?: (\d+))?(?: (\d+))? \"(.+)\"$', user_input):
+            match len(args := [x for x in m.groups() if x is not None]):
+                case 1:
+                    document.insert_line(text=args[0])
+                case 2:
+                    document.insert_line(line_number=int(args[0]), text=args[1])
+                case 3:
+                    document.insert_line(line_number=int(args[0]), column_number=int(args[1]), text=args[2])
+        elif m := re.match(r'delete (\d+)$', user_input):
+            document.delete_line(line_number=int(m.group(1)))
+        elif m := re.match(r'swap (\d+) (\d+)$', user_input):
+            if len(args := [x for x in m.groups() if x is not None]) == 2:
+                document.swap_lines(int(args[0]), int(args[1]))
+        elif user_input == 'undo':
+            document.undo()
+        elif user_input == 'clear':
+            document.clear()
+        elif user_input == 'save':
+            document.save()
+        elif user_input == 'close':
+            document.close()
+        else:
+            print('Error! Unknown command.')
 
 
 class Document:
@@ -38,80 +66,89 @@ class Document:
     def number_of_lines(self) -> int:
         return len(self.current_content)
 
-    def insert_line(self, text: str, line: int = None, column: int = None) -> None:
-        if line is not None and column is not None:
-            if line < 1:
+    def insert_line(self, line_number: int = None, column_number: int = None, text: str = None) -> None:
+        if text is None:
+            sys.exit('Error! You must specify the text of the line you want to insert.')
+        if line_number is not None and column_number is not None:
+            if line_number < 1:
                 sys.exit("Error! You can't insert the line at this position. "
                          'The number of the target line must be a natural number.')
-            elif line > self.number_of_lines:
+            elif line_number > self.number_of_lines:
                 sys.exit("Error! You can't insert the line at this position. "
                          f'The number of lines in the file: {self.number_of_lines}.')
-            elif column < 1:
+            elif column_number < 1:
                 sys.exit("Error! You can't insert the line at this position. "
                          'The number of the target column must be a natural number.')
-            elif column > len(self.current_content[line - 1]):
+            elif column_number > len(self.current_content[line_number - 1]):
                 sys.exit("Error! You can't insert the line at this position. "
-                         f'The number of characters in the target line: {len(self.current_content[line - 1])}.')
+                         f'The number of characters in the target line: {len(self.current_content[line_number - 1])}.')
             else:
                 self.back_up()
-                line_content = self.current_content[line - 1]
-                self.current_content[line - 1] = ''.join((line_content[:column], text, line_content[column:]))
-        elif line is not None:
-            if line < 1:
+                line_content = self.current_content[line_number - 1]
+                self.current_content[line_number - 1] = ''.join(
+                    (line_content[:column_number], text, line_content[column_number:]))
+        elif line_number is not None:
+            if line_number < 1:
                 sys.exit("Error! You can't insert the line at this position. "
                          'The number of the target line must be a natural number.')
-            elif line > self.number_of_lines:
+            elif line_number > self.number_of_lines:
                 sys.exit("Error! You can't insert the line at this position. "
                          f'The number of lines in the file: {self.number_of_lines}.')
             else:
                 self.back_up()
-                self.current_content[line - 1] = self.current_content[line - 1].removesuffix('\n')
-                self.current_content[line - 1] += f'{text}\n'
+                self.current_content[line_number - 1] = self.current_content[line_number - 1].removesuffix('\n')
+                self.current_content[line_number - 1] += f'{text}\n'
         else:
             self.back_up()
             if not self.current_content[-1].endswith('\n'):
                 self.current_content[-1] += '\n'
             self.current_content.append(text)
 
-    def delete_line(self, line: int = None) -> None:
-        if line is None:
+    def delete_line(self, line_number: int = None) -> None:
+        if line_number is None:
             sys.exit('Error! You must specify the number of the line you want to delete.')
-        elif line < 1:
-            sys.exit(f"Error! You can't delete the line №{line}. "
+        elif line_number < 1:
+            sys.exit(f"Error! You can't delete the line №{line_number}. "
                      'The number of the target line must be a natural number.')
-        elif line > self.number_of_lines:
-            sys.exit(f"Error! You can't delete the line №{line}. "
+        elif line_number > self.number_of_lines:
+            sys.exit(f"Error! You can't delete the line №{line_number}. "
                      f'The number of lines in the file: {self.number_of_lines}.')
         else:
             self.back_up()
-            del self.current_content[line - 1]
+            del self.current_content[line_number - 1]
 
-    def swap_lines(self, line1: int, line2: int) -> None:
-        if line1 < 1:
-            sys.exit(f"Error! You can't move the line №{line1}. "
+    def swap_lines(self, line1_number: int, line2_number: int) -> None:
+        if line1_number < 1:
+            sys.exit(f"Error! You can't move the line №{line1_number}. "
                      'The number of the target line must be a natural number.')
-        elif line1 > self.number_of_lines:
-            sys.exit(f"Error! You can't move the line №{line1}. "
+        elif line1_number > self.number_of_lines:
+            sys.exit(f"Error! You can't move the line №{line1_number}. "
                      f'The number of lines in the file: {self.number_of_lines}.')
-        elif line2 < 1:
-            sys.exit(f"Error! You can't move the line №{line1}. "
+        elif line2_number < 1:
+            sys.exit(f"Error! You can't move the line №{line1_number}. "
                      'The number of the target line must be a natural number.')
-        elif line2 > self.number_of_lines:
-            sys.exit(f"Error! You can't move the line №{line2}. "
+        elif line2_number > self.number_of_lines:
+            sys.exit(f"Error! You can't move the line №{line2_number}. "
                      f'The number of lines in the file: {self.number_of_lines}.')
-        elif line1 == line2:
+        elif line1_number == line2_number:
             sys.exit("Error! You can't swap the line with itself.")
         else:
             self.back_up()
-            self.current_content[line1 - 1], self.current_content[line2 - 1] = \
-                self.current_content[line2 - 1], self.current_content[line1 - 1]
+
+            if not self.current_content[line1_number - 1].endswith('\n'):
+                self.current_content[line1_number - 1] += '\n'
+            if not self.current_content[line2_number - 1].endswith('\n'):
+                self.current_content[line2_number - 1] += '\n'
+
+            self.current_content[line1_number - 1], self.current_content[line2_number - 1] = \
+                self.current_content[line2_number - 1], self.current_content[line1_number - 1]
+
+    def undo(self) -> None:
+        self.current_content = copy.deepcopy(self.previous_content)
 
     def clear(self) -> None:
         self.back_up()
         self.current_content.clear()
-
-    def undo(self) -> None:
-        self.current_content = copy.deepcopy(self.previous_content)
 
     def save(self) -> None:
         with self.path.open('w+') as file:
